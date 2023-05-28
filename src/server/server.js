c3811b7deae2e30a6c5e5ff611c0124b87f5be23
kubiatlaura@gmail.com <kubiatlaura@gmail.com>
2023-05-28 14:26:37 +0100

    changed node require to pass

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require("node:path");
+const path = require("path");
 
 const app = express();
 // Setup empty JS object to act as endpoint for all routes
